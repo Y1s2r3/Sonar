c2586206dadff99f192caa3ecac26a1920a868a8
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-19 12:39:34 +0530

    Update test5.groovy

--- test5.groovy
+++ test5.groovy
@@ -4,8 +4,8 @@ node {
   }
   stage('SonarQube analysis') {
    bat "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar \
-  -Dsonar.login=8eca862fd93a92c3e6ae9a1b0b1fcbe627febe73 \
-   -Dsonar.host.url=http://localhost:9000 "
+  -Dsonar.login=e19f2b234afa50cc33d48e0436d19b3c134e3783 \
+   -Dsonar.host.url=https://sonarcloud.io/ "
     
   }
 }

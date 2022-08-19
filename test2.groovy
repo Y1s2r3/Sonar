b98883f4745e1ac606f670342a898cfd2e599d25
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-19 11:06:26 +0530

    Update test2.groovy

--- test2.groovy
+++ test2.groovy
@@ -14,4 +14,25 @@ node {
   -Dsonar.host.url=http://localhost:9000 \
   -Dsonar.login=b28333f90083aa7ab0b59d11dc1c9ba3dc5c1696"
   }
+  stage('upload to nexus') {
+    //bat "mvn verify sonar:sonar"
+    // bat "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=RanjanGitHubb_mark2"
+   
+      nexusArtifactUploader artifacts: [
+      [
+        artifactId: 'mavenforjenkins', 
+        classifier: '', 
+        file: 'target/mavenforjenkins-0.0.1-SNAPSHOT.jar', 
+        type: 'jar'
+      ]
+    ], 
+      credentialsId: 'NEXUS_CRED', 
+      groupId: 'mavenforjenkins', 
+      nexusUrl: 'localhost:8081', 
+      nexusVersion: 'nexus3', 
+      protocol: 'http', 
+      repository: 'Nexus-Test', 
+      version: '0.0.1-SNAPSHOT'
+    
+  }
 }

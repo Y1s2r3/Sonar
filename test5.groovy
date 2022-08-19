29ca68367d852504c9ede853b647389b29c00daa
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-19 12:53:51 +0530

    Update test5.groovy

--- test5.groovy
+++ test5.groovy
@@ -6,4 +6,5 @@ node {
     withSonarQubeEnv(credentialsId: 'e19f2b234afa50cc33d48e0436d19b3c134e3783', installationName: 'ABC') { 
       bat 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
     }
+  }
 }

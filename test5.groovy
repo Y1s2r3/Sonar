96cfbbe3bdef1c14033967986bff9fa5151c2612
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-19 12:01:03 +0530

    Update test5.groovy

--- test5.groovy
+++ test5.groovy
@@ -1,6 +1,6 @@
 node {
   stage('SCM') {
-    git 'cd initial && https://github.com/foo/bar.git'
+    git 'https://github.com/foo/bar.git'
   }
   stage('SonarQube analysis') {
     withSonarQubeEnv(credentialsId: 'a128e9c8ce931305e324e8df042c58ce23077b3f', installationName: 'ABC') { 

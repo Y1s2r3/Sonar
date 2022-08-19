6bd61cafa13c4d22eefa3dda080bf893ccb61802
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-19 12:23:29 +0530

    Update test5.groovy

--- test5.groovy
+++ test5.groovy
@@ -4,11 +4,11 @@ node {
   }
   stage('SonarQube analysis') {
     withSonarQubeEnv(credentialsId: '8eca862fd93a92c3e6ae9a1b0b1fcbe627febe73', installationName: 'ABC') { 
-      "sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar
-      bat "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar \
+     //sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar
+      "bat "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar \
   //-Dsonar.projectKey=wertfgf \
         //-Dsonar.login=8eca862fd93a92c3e6ae9a1b0b1fcbe627febe73
-  -Dsonar.host.url=http://localhost:9000 
+  -Dsonar.host.url=http://localhost:9000 "
     }
   }
 }

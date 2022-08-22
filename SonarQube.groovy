bded54b0014bcdcf3e6be3ae0c30f4dcbf3e67e7
Y1s2r3 <111270654+Y1s2r3@users.noreply.github.com>
2022-08-22 18:35:44 +0530

    Create SonarQube.groovy

---
+++ SonarQube.groovy
@@ -0,0 +1,32 @@
+node {
+  stage('Clone') {
+  git "https://github.com/Y1s2r3/Sonar.git"
+  }
+  stage('Build') {
+    bat "mvn -Dmaven.test.failure.ignore=true clean package"
+  }
+  stage ('analysis') {
+     bat "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=f1b02859e6a66c45d03e242f3bfe2ac79572f30f"
+  }
+  stage('upload to nexus') {
+   
+      nexusArtifactUploader artifacts: [
+      [
+        artifactId: 'mavenforjenkins', 
+        classifier: '', 
+        file: 'target/mavenforjenkins-0.0.1-SNAPSHOT.jar', 
+        type: 'jar'
+      ]
+    ], 
+      credentialsId: 'Nexus-Repo', 
+      groupId: 'mavenforjenkins', 
+      nexusUrl: 'localhost:8081', 
+      nexusVersion: 'nexus3', 
+      protocol: 'http', 
+      repository: 'Nexus-Repo', 
+      version: '0.0.1-SNAPSHOT'
+    
+  }
+}
